f3f72378cd6fb3ed697c85fa80b8ab77b93a5b1b
Aaron Batilo <aaron.batilo@fullcontact.com>
2017-07-26 08:16:18 -0600

    Added a line to remind me to install tmux

--- install.sh
+++ install.sh
@@ -19,3 +19,5 @@ echo "let g:airline_theme='solarized'" >> ~/.vimrc
 # Add an alias for opening a default session file
 grep "alias vis=" ~/.bashrc || echo "alias vis='vi -S ~/.vim/session.vim'" >> ~/.bashrc
 grep "alias vims=" ~/.bashrc || echo "alias vims='vi -S ~/.vim/session.vim'" >> ~/.bashrc
+
+echo "Install tmux then run the tmux.sh"
