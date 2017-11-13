ac7713f7300f5abee940d9b9ada66bc5168674c5
Aaron <AaronBatilo@gmail.com>
2017-11-12 20:22:03 -0700

    Improve installation scripts
    install.sh will now also install to .zshrc and not just .bashrc. tmux.sh will
    now create a symlink instead of copying the file

--- install.sh
+++ install.sh
@@ -12,8 +12,20 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo | vim +PluginInstall +qall
 
 # Add an alias for opening a default session file
+grep -q "set -o vi" ~/.bashrc || echo "set -o vi" >> ~/.bashrc
 grep -q "alias vis=" ~/.bashrc || echo "alias vis='vi -S ~/.vim/session.vim'" >> ~/.bashrc
 grep -q "alias vims=" ~/.bashrc || echo "alias vims='vi -S ~/.vim/session.vim'" >> ~/.bashrc
+grep -q "export EDITOR=vim" ~/.bashrc || echo "export EDITOR=vim" >> ~/.bashrc
+
+if [ -e "$HOME/.zshrc" ]
+then
+  echo "" >> ~/.zshrc
+  grep -q "# vim related" ~/.zshrc || echo "# vim related" >> ~/.zshrc
+  grep -q "set -o vi" ~/.zshrc || echo "set -o vi" >> ~/.zshrc
+  grep -q "alias vis=" ~/.zshrc || echo "alias vis='vi -S ~/.vim/session.vim'" >> ~/.zshrc
+  grep -q "alias vims=" ~/.zshrc || echo "alias vims='vi -S ~/.vim/session.vim'" >> ~/.zshrc
+  grep -q "export EDITOR=vim" ~/.zshrc || echo "export EDITOR=vim" >> ~/.zshrc
+fi
 
 echo "Install tmux then run the tmux.sh"
 
--- tmux.sh
+++ tmux.sh
@@ -3,8 +3,8 @@
 rm -rf ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Move the tmux config file to where it will be looked for
-cp .tmux.conf ~/.tmux.conf
+# Soft link the tmux config file to where it will be looked for
+ln -s "$PWD/.tmux.conf" ~/.tmux.conf
 
 # Reminder on how to install the plugins
 echo "Open a tmux session and hit C-b + I to install the plugins"
