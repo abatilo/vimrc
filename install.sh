ad29a59ec277942deb120508c0557158e774133d
Aaron <AaronBatilo@gmail.com>
2018-03-10 19:58:04 -0700

    Add some wonderful FZF settings

--- install.sh
+++ install.sh
@@ -58,6 +58,18 @@ then
   grep -q "alias vi=" ~/.zshrc || echo "alias vi='nvim'" >> ~/.zshrc
   grep -q "alias vim=" ~/.zshrc || echo "alias vim='nvim'" >> ~/.zshrc
   grep -q "export EDITOR=nvim" ~/.zshrc || echo "export EDITOR=nvim" >> ~/.zshrc
+
+  grep -q "# Use ripgrep for fzf" ~/.zshrc || echo "# Use ripgrep for fzf" >> ~/.zshrc
+  grep -q "export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g \"!{.git,node_modules}/*\" 2> /dev/null'" ~/.zshrc || echo "export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g \"!{.git,node_modules}/*\" 2> /dev/null'" >> ~/.zshrc
+  grep -q 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' ~/.zshrc || echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >> ~/.zshrc
+  grep -q "ctrlp() { </dev/tty vim -c CtrlP }" ~/.zshrc || echo "ctrlp() { </dev/tty vim -c CtrlP }" >> ~/.zshrc
+  grep -q "zle -N ctrlp" ~/.zshrc || echo "zle -N ctrlp" >> ~/.zshrc
+  grep -q "bindkey \"^p\" ctrlp" ~/.zshrc || echo "bindkey \"^p\" ctrlp" >> ~/.zshrc
+
+  grep -q "# Set 'infinite' zsh history" ~/.zshrc || echo "# Set 'infinite' zsh history" >> ~/.zshrc
+  grep -q "HISTFILE=~/.zsh_history" ~/.zshrc || echo "HISTFILE=~/.zsh_history" >> ~/.zshrc
+  grep -q "HISTSIZE=999999999" ~/.zshrc || echo "HISTSIZE=999999999" >> ~/.zshrc
+  grep -q "SAVEHIST=$HISTSIZE" ~/.zshrc || echo "SAVEHIST=$HISTSIZE" >> ~/.zshrc
 fi
 
 echo "Install tmux then run the tmux.sh"
