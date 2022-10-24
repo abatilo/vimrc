dfbed7e204e92b3310edb5122d4b2febce98b78c
Aaron Batilo <AaronBatilo@gmail.com>
2022-10-24 08:03:06 -0600

    Update from deprecated nvim_lsp method

--- nvim/init.lua
+++ nvim/init.lua
@@ -249,7 +249,7 @@ end
 
 local function config(_config)
 	return vim.tbl_deep_extend("force", {
-		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 		on_attach = on_attach,
 	}, _config or {})
 end
