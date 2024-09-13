45c4ef56549e38ab54c90f604fa2d1642c0072aa
Aaron Batilo <AaronBatilo@gmail.com>
2024-09-13 10:14:58 -0600

    Use avante again

--- nvim/init.lua
+++ nvim/init.lua
@@ -234,7 +234,7 @@ require("lazy").setup({
         "pyright",
         "tailwindcss",
         "terraformls",
-        "tsserver",
+        "ts_ls",
         "yamlls",
       }
 
@@ -344,27 +344,47 @@ require("lazy").setup({
     end,
   },
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    build = "make",
-    opts = {
-      -- add any opts here
-    },
-    dependencies = {
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below is optional, make sure to setup it properly if you have lazy=true
-      {
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  opts = {
+    -- add any opts here
+  },
+  build = ":AvanteBuild", -- This is optional, recommended tho. Also note that this will block the startup for a bit since we are compiling bindings in Rust.
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
         },
-        ft = { "markdown", "Avante" },
       },
     },
-  }
+    {
+      -- Make sure to setup it properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
 })
 
 -- Decide where the root of a project is
