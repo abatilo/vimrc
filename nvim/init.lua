a2ec3aa312eeb3c931113bb67c9f7916d19c8b6c
Aaron Batilo <AaronBatilo@gmail.com>
2024-10-10 15:09:14 -0600

    Fix indentation for deepseek configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -403,35 +403,35 @@ require("lazy").setup({
       },
       vendors = {
       ---@type AvanteProvider
-      deepseek = {
-        endpoint = "https://deepseek.33ca82-shanks.coreweave.app/v1/chat/completions",
-        model = "deepseek-coder",
-        api_key_name = "TGI_API_KEY",
-        parse_curl_args = function(opts, code_opts)
-          return {
-            url = opts.endpoint,
-            headers = {
-              ["Accept"] = "application/json",
-              ["Content-Type"] = "application/json",
-              ["Authorization"] = "Basic " .. os.getenv(opts.api_key_name),
-            },
-            body = {
-              model = opts.model,
-              messages = { -- you can make your own message, but this is very advanced
-                { role = "system", content = code_opts.system_prompt },
-                { role = "user", content = require("avante.providers.openai").get_user_message(code_opts) },
+        deepseek = {
+          endpoint = "https://deepseek.33ca82-shanks.coreweave.app/v1/chat/completions",
+          model = "deepseek-coder",
+          api_key_name = "TGI_API_KEY",
+          parse_curl_args = function(opts, code_opts)
+            return {
+              url = opts.endpoint,
+              headers = {
+                ["Accept"] = "application/json",
+                ["Content-Type"] = "application/json",
+                ["Authorization"] = "Basic " .. os.getenv(opts.api_key_name),
               },
-              temperature = 0,
-              max_tokens = 2048,
-              stream = true, -- this will be set by default.
-            },
-          }
-        end,
-        parse_response_data = function(data_stream, event_state, opts)
-          require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-        end,
-      },
-    }
+              body = {
+                model = opts.model,
+                messages = { -- you can make your own message, but this is very advanced
+                  { role = "system", content = code_opts.system_prompt },
+                  { role = "user", content = require("avante.providers.openai").get_user_message(code_opts) },
+                },
+                temperature = 0,
+                max_tokens = 7168,
+                stream = true, -- this will be set by default.
+              },
+            }
+          end,
+          parse_response_data = function(data_stream, event_state, opts)
+            require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+          end,
+        },
+      }
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
