ed4d9f4d1a923aeae7eb2fcff452677d371f9a7f
Aaron Batilo <AaronBatilo@gmail.com>
2024-10-28 07:43:14 -0600

    Set global aider config

--- install.sh
+++ install.sh
@@ -24,6 +24,10 @@ ln -s "$PWD/.gitconfig_global" ~/.gitconfig
 # Set a default global .gitignore
 ln -s "$PWD/.gitignore_global" ~/.gitignore
 
+# Set aidor files
+ln -s "$PWD/.aider.conf.yml" ~/.aider.conf.yml
+ln -s "$PWD/.aider.model.metadata.json" ~/.aider.model.metadata.json
+
 # Install a default global .editorconfig
 ln -s "$PWD/.editorconfig_global" ~/.editorconfig
 
@@ -33,25 +37,25 @@ ln -s "$PWD/alacritty.yml" ~/.config/alacritty/alacritty.yml
 # Version 0.13 and later uses a toml file
 ln -s "$PWD/alacritty.toml" ~/.config/alacritty/alacritty.toml
 
-echo "" >> ~/.zshrc
-grep -q "# vim related" ~/.zshrc || echo "# vim related" >> ~/.zshrc
-grep -q "set -o vi" ~/.zshrc || echo "set -o vi" >> ~/.zshrc
-grep -q "alias vi=" ~/.zshrc || echo "alias vi='nvim'" >> ~/.zshrc
-grep -q "alias vim=" ~/.zshrc || echo "alias vim='nvim'" >> ~/.zshrc
-grep -q "export EDITOR=nvim" ~/.zshrc || echo "export EDITOR=nvim" >> ~/.zshrc
+echo "" >>~/.zshrc
+grep -q "# vim related" ~/.zshrc || echo "# vim related" >>~/.zshrc
+grep -q "set -o vi" ~/.zshrc || echo "set -o vi" >>~/.zshrc
+grep -q "alias vi=" ~/.zshrc || echo "alias vi='nvim'" >>~/.zshrc
+grep -q "alias vim=" ~/.zshrc || echo "alias vim='nvim'" >>~/.zshrc
+grep -q "export EDITOR=nvim" ~/.zshrc || echo "export EDITOR=nvim" >>~/.zshrc
 
-grep -q "# Use ripgrep for fzf" ~/.zshrc || echo "# Use ripgrep for fzf" >> ~/.zshrc
-grep -q "export FZF_DEFAULT_COMMAND=" ~/.zshrc || echo "export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g \"!{.git,node_modules}/*\" 2> /dev/null'" >> ~/.zshrc
-grep -q 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' ~/.zshrc || echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >> ~/.zshrc
+grep -q "# Use ripgrep for fzf" ~/.zshrc || echo "# Use ripgrep for fzf" >>~/.zshrc
+grep -q "export FZF_DEFAULT_COMMAND=" ~/.zshrc || echo "export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g \"!{.git,node_modules}/*\" 2> /dev/null'" >>~/.zshrc
+grep -q 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' ~/.zshrc || echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >>~/.zshrc
 
-grep -q "# Set 'infinite' zsh history" ~/.zshrc || echo "# Set 'infinite' zsh history" >> ~/.zshrc
-grep -q "HISTFILE=~/.zsh_history" ~/.zshrc || echo "HISTFILE=~/.zsh_history" >> ~/.zshrc
-grep -q "HISTSIZE=100000" ~/.zshrc || echo "HISTSIZE=100000" >> ~/.zshrc
-grep -q "SAVEHIST=$HISTSIZE" ~/.zshrc || echo "SAVEHIST=$HISTSIZE" >> ~/.zshrc
-grep -q "setopt appendhistory" ~/.zshrc || echo "setopt appendhistory" >> ~/.zshrc
+grep -q "# Set 'infinite' zsh history" ~/.zshrc || echo "# Set 'infinite' zsh history" >>~/.zshrc
+grep -q "HISTFILE=~/.zsh_history" ~/.zshrc || echo "HISTFILE=~/.zsh_history" >>~/.zshrc
+grep -q "HISTSIZE=100000" ~/.zshrc || echo "HISTSIZE=100000" >>~/.zshrc
+grep -q "SAVEHIST=$HISTSIZE" ~/.zshrc || echo "SAVEHIST=$HISTSIZE" >>~/.zshrc
+grep -q "setopt appendhistory" ~/.zshrc || echo "setopt appendhistory" >>~/.zshrc
 
-grep -q "octo()" ~/.zshrc || echo "octo() { vim -c \"Octo pr edit \$1\" }" >> ~/.zshrc
-grep -q "export PAGER=" ~/.zshrc || echo "export PAGER=" >> ~/.zshrc
+grep -q "octo()" ~/.zshrc || echo "octo() { vim -c \"Octo pr edit \$1\" }" >>~/.zshrc
+grep -q "export PAGER=" ~/.zshrc || echo "export PAGER=" >>~/.zshrc
 
 echo "Install tmux then run the tmux.sh"
 
