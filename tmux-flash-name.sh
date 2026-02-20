8e64ac16c0c9f16b1926a64c4157f49af17073f2
Aaron Batilo <AaronBatilo@gmail.com>
2026-02-20 08:59:55 -0700

    chore(hooks): remove tmux flash hook and update notification sound

--- tmux-flash-name.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Flash the tmux window name then restore after 2 seconds.
-# Designed to run async from a Claude Code Stop hook.
-
-ORIGINAL_NAME=$(tmux display-message -p '#W')
-tmux rename-window "Finished!"
-sleep 2
-tmux rename-window "$ORIGINAL_NAME"
